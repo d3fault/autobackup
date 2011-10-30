7d2b82c44d5226605ebfe1f35a159ec143eff691
d3fault <d3fault@d3fault.d3fault>
2011-10-30 05:47:32 -0700

    Auto-Commit @ Sun Oct 30 05:47:32 USMST 2011

--- autobackupScripts/git.add.commit.and.push.bat
+++ autobackupScripts/git.add.commit.and.push.bat
@@ -3,4 +3,3 @@ C:
 chdir C:\cygwin\bin
 bash --login -i -c "cd /cygdrive/c/autobackup/autobackupScripts/; sh ./git.cygwin.commands.sh; exit;"
 echo added, commited, and pushed
-pause
--- autobackupScripts/my.shutdown.bat
+++ autobackupScripts/my.shutdown.bat
@@ -1,6 +1,9 @@
 @echo off
+echo Syncing with 750gb drives...
+call git.add.commit.and.push.bat
+echo ...750gb drives synced
 echo Dismounting 3x 750gb drives...
 call dismount.3x.750gb.drives.bat
 echo ...3x 750gb drives dismounted
 echo Shutting Down
-shutdown /s /t 1
+shutdown /s /t 0
