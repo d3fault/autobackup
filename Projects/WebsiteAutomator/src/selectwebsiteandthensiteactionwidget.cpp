9fc729d09a10a93c5910301edddc3054c5281182
d3fault <d3fault@d3fault.d3fault>
2011-10-31 06:29:42 -0700

    Auto-Commit @ Mon Oct 31 06:29:42 USMST 2011

--- Projects/WebsiteAutomator/src/selectwebsiteandthensiteactionwidget.cpp
+++ Projects/WebsiteAutomator/src/selectwebsiteandthensiteactionwidget.cpp
@@ -22,13 +22,15 @@ selectWebsiteAndThenSiteActionWidget::selectWebsiteAndThenSiteActionWidget(QWidg
     Step *submitStep = new Step("some parcellizeable way of signifying that we want to fill out the name, email address, body, and image field (the captcha is implicit/automatic)");
     //i could put each input "name" and "email address" etc as it's own step, but then it might fuck up the automated captcha stuff because i'd be checking for it's existence after each and every step (well, before and after each, i think)
     postThreadAction.addStep(submitStep);
-    fourChanWebsite.addAction(postThreadAction);
+    fourChanWebsite.Actions.append(postThreadAction);
     Websites.append(fourChanWebsite);
     //TODOreq: fourchanWebsite needs to be parcelable, which parcelizes all the actions and all their steps...
     //technically, it need only be read-only parcelable... we populate the object from a parcelized version pulled from the site..
     //..but to future proof it (when i outsource the creation of definitions), i should make it both readwrite parcelable
 
     //TODO: idk where this would go, but there needs to be like "variable chaining". one of the variables when posting to craigslist is a random string used to identify an input field. you must first parse out the input field name (random string), then you must also attach a piece of input to that input field name for submission.. a 2 step process. complicates shit even more. my brain is seriously going to explode considering all these possibilities
+
+    //as much as i want to write a simplified case... say, posting to 4chan... i want to make sure the design is scalable as fuck to any/all scenarios...
 }
 selectWebsiteAndThenSiteActionWidget::~selectWebsiteAndThenSiteActionWidget()
 {
