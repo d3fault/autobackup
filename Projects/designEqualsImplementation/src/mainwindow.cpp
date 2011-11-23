b57ba77e04c829a9456365c17a82283519fcdfa1
d3fault <d3fault@d3fault.d3fault>
2011-11-23 13:46:06 -0700

    Auto-Commit @ Wed Nov 23 13:46:06 MST 2011

--- Projects/designEqualsImplementation/src/mainwindow.cpp
+++ Projects/designEqualsImplementation/src/mainwindow.cpp
@@ -82,16 +82,25 @@ void MainWindow::createLeftPane()
 }
 void MainWindow::createNodeButtonGroups()
 {
+    //TODOmb: reverse the output from uniqeKeys() and values() below, otherwise it'll be all backwards from how i want it (not a huge deal)
     //TODOreq: left off here, re-arranged DesignProject... but i want some way to iterate through ProjectViewTypes to make 2("x") tabs here, and another way to iterate through each nodeType for each viewType to create the buttonGroup (2 colums, x rows (depending how many)) for each viewType/tab
     DesignProjectTemplates *dpt = new DesignProjectTemplates();
+    //^i'm thinking i can attach signals to this such as "onViewTypeAdded" and "onNodeAdded" (which has the viewType as an argument (how do i access a tab based on the viewType? can't i only access them via integer? i guess i can iterate them, since there's only 2 atm...))
+    //^^if i end up doing that, all/most code below is gone/relocated
+    //also, i need to connect to dpt signals before issuing a "createTemplates()" command to it (currently in it's constructor, no time to hook up connections
+    //i could fill out the tab/widget's layout with the NodeButton groupbox's etc before i emit the "onViewTypeAdded", so i don't have to even deal with onNodeAdded and iterating through the tabs to find what viewType the node is associated with
     QList<DesignProjectTemplates::DesignProjectViewType> i = dpt->AllDesignProjectNodesByProjectViewType->uniqueKeys();
     int viewTypesLength = i.length();
     for(int j = 0; j < viewTypesLength; ++j)
     {
         //add tab for this DesignProjectViewType (class diagram, use case, etc)
-        QList<DiagramSceneNode*> dpt->AllDesignProjectNodesByProjectViewType->values(i.at(j));
-        //add a button to the groupbox for this tab.. maybe do other stuff (look at that handy example)... connect to it's signals/slots etc?
 
+        QList<DiagramSceneNode*> NodesOfThisViewType = dpt->AllDesignProjectNodesByProjectViewType->values(i.at(j));
+        int nodesOfThisViewTypeCount = NodesOfThisViewType.length();
+        for(int k = 0; k < nodesOfThisViewTypeCount; ++k)
+        {
+            //add a button to the groupbox for this tab.. maybe do other stuff (look at that handy example)... connect to it's signals/slots etc?
+        }
     }
 
 
