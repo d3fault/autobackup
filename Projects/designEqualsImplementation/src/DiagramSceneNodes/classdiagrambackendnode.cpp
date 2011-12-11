d2ed423e614f877fc12caf372980cfd51d8b28d1
d3fault <d3fault@d3fault.d3fault>
2011-12-10 20:40:38 -0700

    Auto-Commit @ Sat Dec 10 20:40:38 MST 2011

--- Projects/designEqualsImplementation/src/DiagramSceneNodes/classdiagrambackendnode.cpp
+++ Projects/designEqualsImplementation/src/DiagramSceneNodes/classdiagrambackendnode.cpp
@@ -12,7 +12,6 @@ QString ClassDiagramBackEndNode::getNodeTypeAsString()
 }
 void ClassDiagramBackEndNode::drawMyPolygon()
 {
-    m_MyPolygon << QPointF(-100, 0) << QPointF(0, 100)
-                          << QPointF(100, 0) << QPointF(0, -100)
-                          << QPointF(-100, 0);
+    //m_MyPolygon << QPointF(-100, 0) << QPointF(0, 100) << QPointF(100, 0) << QPointF(0, -100) << QPointF(-100, 0);
+    m_MyPolygon << QPointF(-100, -100) << QPointF(100, -100) << QPointF(100, 100) << QPointF(-100, 100);
 }
