4fd2ee55628375e7fab1bc263f17dde4e662159e
d3fault <d3fault@d3fault.d3fault>
2011-12-26 10:19:19 -0700

    libAvAudioPlayer2 nonworking compiling checkpoint. audio code in place

--- Projects/Prototypes/libAvAudioPlayer2/src/qtaudioplayer.cpp
+++ Projects/Prototypes/libAvAudioPlayer2/src/qtaudioplayer.cpp
@@ -1,15 +1,84 @@
 #include "qtaudioplayer.h"
 
 QtAudioPlayer::QtAudioPlayer(QObject *parent) :
-    QObject(parent)
+    QObject(parent), m_Started(false), m_SpecSet(false)
 {
 }
 void QtAudioPlayer::init()
 {
+    //nothing to do here because we need an audio spec!
 }
 void QtAudioPlayer::setAudioSpec(int sampleRate, int numChannels, int sampleSize)
 {
+    m_Format.setFrequency(sampleRate);
+    m_Format.setChannelCount(numChannels);
+    m_Format.setSampleSize(sampleSize);
+    m_Format.setCodec("audio/pcm");
+    m_Format.setByteOrder(QAudioFormat::LittleEndian); //TODO: dunno. we should DETECT it really...
+    m_Format.setSampleType(QAudioFormat::SignedInt);
+
+    QAudioDeviceInfo info(QAudioDeviceInfo::defaultOutputDevice());
+    if (!info.isFormatSupported(m_Format))
+    {
+        m_Format = info.nearestFormat(m_Format);
+    }
+
+    m_AudioOutput = new QAudioOutput(QAudioDeviceInfo::defaultOutputDevice(), m_Format);
+    m_Timer = new QTimer();
+    connect(m_Timer, SIGNAL(timeout()), this, SLOT(fillAudioBuffer()));
+    m_AudioBuffer = m_AudioOutput->start();
+
+    m_SpecSet = true;
+    if(m_Started)
+    {
+        actualStart();
+    }
 }
 void QtAudioPlayer::handleNewAudioBytes(QByteArray newAudioBytes)
 {
+    if (m_AudioOutput && m_AudioOutput->state() != QAudio::StoppedState)
+    {
+        int chunks = newAudioBytes.size()/m_AudioOutput->periodSize();
+        while (chunks)
+        {
+           QByteArray buffer = newAudioBytes.left(m_AudioOutput->periodSize());
+           int bufferSize = buffer.size();
+           if(bufferSize < newAudioBytes.size())
+               newAudioBytes.remove(0, bufferSize);
+           if (bufferSize > 0)
+               m_AudioBuffer->write(buffer, buffer.size());
+           if (bufferSize != m_AudioOutput->periodSize())
+           {
+               break;
+           }
+           --chunks;
+        }
+    }
+    else
+    {
+        m_Timer->stop();
+    }
+}
+void QtAudioPlayer::play()
+{
+    m_Started = true;
+    if(m_SpecSet)
+    {
+        actualStart();
+    }
+}
+void QtAudioPlayer::actualStart()
+{
+    m_Timer->start(20);
+}
+void QtAudioPlayer::fillAudioBuffer()
+{
+    if (m_AudioOutput && m_AudioOutput->state() != QAudio::StoppedState)
+    {
+        emit needAudio(m_AudioOutput->bytesFree());
+    }
+    else
+    {
+        m_Timer->stop();
+    }
 }
--- Projects/Prototypes/libAvAudioPlayer2/src/qtaudioplayer.h
+++ Projects/Prototypes/libAvAudioPlayer2/src/qtaudioplayer.h
@@ -2,18 +2,33 @@
 #define QTAUDIOPLAYER_H
 
 #include <QObject>
+#include <QtMultimedia/QAudioOutput>
+#include <QIODevice>
+#include <QTimer>
 
 class QtAudioPlayer : public QObject
 {
     Q_OBJECT
 public:
     explicit QtAudioPlayer(QObject *parent = 0);
+private:
+    void actualStart();
+    bool m_Started;
+    bool m_SpecSet;
+
+    QAudioOutput *m_AudioOutput;
+    QAudioFormat m_Format;
+    QIODevice* m_AudioBuffer;
+    QTimer *m_Timer;
 signals:
     void needAudio(int bytesNeeded);
 public slots:
     void init();
+    void play();
     void setAudioSpec(int sampleRate, int numChannels, int sampleSize);
     void handleNewAudioBytes(QByteArray newAudioBytes);
+private slots:
+    void fillAudioBuffer();
 };
 
 #endif // QTAUDIOPLAYER_H
