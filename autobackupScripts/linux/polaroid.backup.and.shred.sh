7f0226343bb6fc8061065443f626f0f7d88d5ac0
d3fault <d3fault@d3fault.d3fault>
2011-12-28 11:44:36 -0700

    Auto-Commit @ Wed Dec 28 11:44:36 MST 2011

---
+++ autobackupScripts/linux/polaroid.backup.and.shred.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+read -n1 -r -p "make sure the device is mounted in: /media/9CB5-7BF4/" key
+cd /media/9CB5-7BF4/DCIM/200MOVIE/
+
+epochTime=`date +%s`
+newDir=syncAt`echo $epochTime`
+new750x1=/mnt/750x1/polaroidVideo/`echo $newDir`/
+new750x2=/mnt/750x2/polaroidVideo/`echo $newDir`/
+
+sudo mkdir `echo $new750x1`
+sudo mkdir `echo $new750x2`
+
+sudo cp -av ./* "$new750x1"
+sudo cp -av ./* "$new750x2"
+
+sudo shred -fuv --iterations=1 ./*
