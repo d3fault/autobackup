6646bcbe039b6ccca319d9845360337f1ce51d3f
d3fault <d3fault@d3fault.d3fault>
2012-01-03 14:04:40 -0700

    Auto-Commit @ Tue Jan  3 14:04:40 MST 2012

--- Projects/Prototypes/libAvAudioPlayer2/src/synchronizer.cpp
+++ Projects/Prototypes/libAvAudioPlayer2/src/synchronizer.cpp
@@ -6,7 +6,7 @@ Synchronizer::Synchronizer(QObject *parent) :
 }
 void Synchronizer::audioTimeUpdated(quint64 microsecondsElapsed)
 {
-    Q_UNUSED(microsecondsElapsed);
+    emit d("microseconds played: " + QString::number(microsecondsElapsed));
     //TODO: math looking at pts for video frames etc to determine if we should emit showFrame(QVideoFrame)
     //this class is proving to be quite worthless without video incorporated
 }
