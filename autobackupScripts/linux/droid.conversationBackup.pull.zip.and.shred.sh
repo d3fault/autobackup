7a23b67d91268831be239da7e3671dd551ea326b
d3fault <d3fault@d3fault.d3fault>
2012-03-31 07:11:50 -0700

    added autobackup script to pull my conversation backup zip files from droid and shred them

---
+++ autobackupScripts/linux/droid.conversationBackup.pull.zip.and.shred.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+read -n1 -r -p "make sure the device is mounted in: /media/FF64-0C05/" key
+cd /media/FF64-0C05/Android/data/net.ugorji.android.conversationbackup/files/
+cp -av ./*.zip /mnt/750x1/droid/conversationBackup/
+cp -av ./*.zip /mnt/750x2/droid/conversationBackup/
+read -n1 -r -p "done copying, press a key to start shredding" key
+shred -fuv --iterations=1 ./*.zip
