76e2d9980757e0bb799011081b33e28ea7c73cf8
d3fault <d3fault@d3fault.d3fault>
2012-04-05 14:59:23 -0700

    menu isn't lazy loading, i'm just going to scratch the menu and use WAnchor's instead. the Menus barely give me any functionality anyways... just complicate things

--- Solutions/Anonymous Bitcoin Computing/src/views/balance/abcbalanceaddfunds.cpp
+++ Solutions/Anonymous Bitcoin Computing/src/views/balance/abcbalanceaddfunds.cpp
@@ -3,4 +3,47 @@
 AbcBalanceAddFunds::AbcBalanceAddFunds(WContainerWidget *parent)
     : WContainerWidget(parent)
 {
+    addWidget(new WLabel("test123"));
+    /*//Key
+    WLabel *addFundsKeyLabel = new WLabel("Key: ");
+    m_AddFundsKeyLineEdit = new WLineEdit("[None]");
+    m_AddFundsKeyLineEdit->setReadOnly(true);
+    addFundsKeyLabel->setBuddy(m_AddFundsKeyLineEdit);
+
+
+    //Pending Payment Amount
+    WLabel *pendingPaymentAmountLabel = new WLabel("Pending Amount: ");
+    m_PendingPaymentAmountActual = new WLabel("0.00000000");
+
+    //Confirmed Payment Amount
+    WLabel *confirmedPaymentAmountLabel = new WLabel("Confirmed Amount: ");
+    m_ConfirmedPaymentAmountActual = new WLabel("0.00000000");
+
+    //Request Add Funds Key Pushbutton
+    WPushButton *requestAddFundsButton = new WPushButton("Request Add Funds Key");
+    requestAddFundsButton->clicked().connect(this, &AbcBalanceAddFunds::handleAddFundsBalanceButtonClicked);
+
+
+    //Layout
+    WGridLayout *layout = new WGridLayout();
+
+    layout->addWidget(addFundsKeyLabel, 0, 0);
+    layout->addWidget(m_AddFundsKeyLineEdit, 0, 1);
+
+    layout->addWidget(pendingPaymentAmountLabel, 1, 0);
+    layout->addWidget(m_PendingPaymentAmountActual, 1, 1);
+
+    layout->addWidget(confirmedPaymentAmountLabel, 2, 0);
+    layout->addWidget(confirmedPaymentAmountLabel, 2, 1);
+
+    layout->addWidget(requestAddFundsButton, 3, 1);
+
+    this->setLayout(layout);*/
+}
+void AbcBalanceAddFunds::handleAddFundsBalanceButtonClicked()
+{
+    /*
+    WApplication::instance()->deferRendering(); //freeze the UI until we get our response
+    QMetaObject::invokeMethod(AbcAppDbHelper::Instance(), "addFundsRequest", Qt::QueuedConnection, Q_ARG(QString,username)); //maybe also send in our Wt SessionID and a Callback?
+    */
 }
--- Solutions/Anonymous Bitcoin Computing/src/views/balance/abcbalanceaddfunds.h
+++ Solutions/Anonymous Bitcoin Computing/src/views/balance/abcbalanceaddfunds.h
@@ -1,13 +1,25 @@
 #ifndef ABCBALANCEADDFUNDS_H
 #define ABCBALANCEADDFUNDS_H
 
+#include <Wt/WApplication>
 #include <Wt/WContainerWidget>
+#include <Wt/WGridLayout>
+#include <Wt/WLineEdit>
+#include <Wt/WLabel>
+#include <Wt/WBreak>
+#include <Wt/WPushButton>
 using namespace Wt;
 
 class AbcBalanceAddFunds : public WContainerWidget
 {
 public:
     AbcBalanceAddFunds(WContainerWidget *parent = 0);
+private:
+    WLineEdit *m_AddFundsKeyLineEdit;
+    WLabel *m_PendingPaymentAmountActual;
+    WLabel *m_ConfirmedPaymentAmountActual;
+
+    void handleAddFundsBalanceButtonClicked();
 };
 
 #endif // ABCBALANCEADDFUNDS_H
