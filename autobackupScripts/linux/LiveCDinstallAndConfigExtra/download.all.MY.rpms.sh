d58600e4e30adc869e9d3f134ecc9975904655d1
d3fault <d3fault@d3fault.d3fault>
2012-05-26 16:39:20 -0700

    Auto-Commit @ Sat May 26 16:39:20 MST 2012

--- autobackupScripts/linux/LiveCDinstallAndConfigExtra/download.all.MY.rpms.sh
+++ autobackupScripts/linux/LiveCDinstallAndConfigExtra/download.all.MY.rpms.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# assumes rpmfusion already installed
+# assumes rpmfusion repo and adobe repo already installed. you can remove adobe repo probably, unless you want flash updates. actually yea i'm gonna fuck that shit. wait no the install --downloadonly won't work without it in the repo list. fml
 yum install yum-downloadonly
-yum install keepassx ntp vagalume gnome-games mplayer firefox gstreamer-plugins-ugly gstreamer-plugins-bad -yv --downloadonly --downloaddir=./
+yum install p7zip flash-plugin mozilla-adblockplus mozilla-noscript alsa-plugins-pulseaudio nspluginwrapper keepassx ntp vagalume gnome-games mplayer firefox gstreamer-plugins-ugly gstreamer-plugins-bad -y -v --downloadonly --downloaddir=./
--- autobackupScripts/linux/LiveCDinstallAndConfigExtra/install.all.rpms.via.yum.and.configure.extra.sh
+++ autobackupScripts/linux/LiveCDinstallAndConfigExtra/install.all.rpms.via.yum.and.configure.extra.sh
@@ -13,16 +13,28 @@ chmod 777 /home/liveuser/.vagalume/config.xml
 
 # date, time, and timezone
 cp /usr/share/zoneinfo/America/Phoenix /etc/localtime
+echo "getting the date"
 ntpdate 0.fedora.pool.ntp.org
+echo "got the date"
 
 # USER MODE SETTINGS. PERFORM ALL ROOT OPERATIONS BEFORE THIS LINE nvm doesn't work
 # su liveuser
 
 # desktop, a preinstalled image specific to fedora 17 lxde
+echo "changing background"
 su liveuser --command="pcmanfm --set-wallpaper=/usr/share/backgrounds/images/ladybugs.jpg"
+echo "background changed"
 
 # firefox, create new profile, append custom config -- assumes noscript + adblock installed
+echo "Creating and customizing a Firefox profile"
 su liveuser --command="/usr/bin/firefox -CreateProfile default"
 su liveuser --command="cat ./firefox.prefs.additions.txt >> /home/liveuser/.mozilla/firefox/*.default/prefs.js"
+echo "Done Creating and customizing a Firefox profile"
 
-
+# keepassx
+echo "Configuring keepassx"
+su liveuser --command="mkdir -p /home/liveuser/.config/keepassx/"
+su liveuser --command="cp ./keepassx.config.ini /home/liveuser/.config/keepassx/config.ini"
+echo "Done configuring keepassx"
+echo "-"
+echo "install/configure script done"
