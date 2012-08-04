f8eb0cba9156471451f6b4372bf6c974dcea998e
d3fault <d3fault@d3fault.d3fault>
2012-08-04 09:01:27 -0700

    Auto-Commit @ Sat Aug  4 09:01:27 MST 2012

--- autobackupScripts/linux/droid.backup.and.shred.sh
+++ autobackupScripts/linux/droid.backup.and.shred.sh
@@ -5,8 +5,8 @@ cp -av ./ /mnt/750x1/droid/camera/
 cp -av ./ /mnt/750x2/droid/camera/
 cd ..
 cd ..
-cp -av ./recording-*.3gp /mnt/750x1/droidSoundRecorder/
-cp -av ./recording-*.3gp /mnt/750x2/droidSoundRecorder/
+cp -av ./recording*.3gpp /mnt/750x1/droidSoundRecorder/
+cp -av ./recording*.3gpp /mnt/750x2/droidSoundRecorder/
 cd ./DCIM/Camera/
 read -n1 -r -p "done copying, press a key to start shredding" key
 shred -fuv --iterations=1 ./*
@@ -15,5 +15,5 @@ cd ./.thumbnails/
 shred -fuv --iterations=1 ./*
 cd ..
 cd ..
-shred -fuv --iterations=1 ./recording-*.3gp
+shred -fuv --iterations=1 ./recording*.3gpp
 
