89770b9a51602896dd3459b30b0747f423826576
d3fault <d3fault@localhost.localdomain>
2013-12-21 17:04:25 -0700

    Fix 64bit build folders showing up in .lastModifiedTimestamps file (guh, it keeps changing!)

--- Projects/QuickDirtyAutoBackupHalper/src/mainwidget.cpp
+++ Projects/QuickDirtyAutoBackupHalper/src/mainwidget.cpp
@@ -148,7 +148,7 @@ void mainWidget::setupGui()
     m_FilenameIgnoreLineEdit = new QLineEdit();
     m_FilenamesEndsWithIgnoreLineEdit = new QLineEdit(".pro.user,~");
     m_DirnameIgnoreLineEdit = new QLineEdit(".git");
-    m_DirnameEndsWithIgnoreLineEdit = new QLineEdit("_Qt_5_2_0_GCC_32bit-Release,_Qt_5_2_0_GCC_32bit-Debug");
+    m_DirnameEndsWithIgnoreLineEdit = new QLineEdit("_Qt_5_2_0_GCC_32bit-Release,_Qt_5_2_0_GCC_32bit-Debug,_Qt_5_2_0_GCC_64bit-Debug,_Qt_5_2_0_GCC_64bit-Release");
     m_PushToGitIgnoreButton = new QPushButton("Push To .gitignore"); //because i don't want it to be rewritten every time, and detecting changes is too hard
     m_WorkingDirectoryLineEdit = new QLineEdit("/home/d3fault/autobackup");
     m_MountPointSubDirToBareGitRepoLineEdit = new QLineEdit("autobackup"); //TODOoptional: customizable/persist'able. we append this sub-dir to each mount point when doing git push. we might additionally want to be able to specify the "working dir" (the FULL dir, not just sub-dir)... and hell this could even transform into "profiles" (one working dir has many tc containers and a configured subdir specific to that one working dir). for now hard-coded and single 'profile' is fine
