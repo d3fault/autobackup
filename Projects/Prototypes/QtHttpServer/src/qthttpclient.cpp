16faa582c648c7e937c62790e19ef4dcdf007478
d3fault <d3fault@localhost.localdomain>
2014-12-29 18:05:46 -0700

    QtHttpServer: Had fun parsing the url query using Qt. Man that Boost ASIO http server example is so much larger than this simple shit

--- Projects/Prototypes/QtHttpServer/src/qthttpclient.cpp
+++ Projects/Prototypes/QtHttpServer/src/qthttpclient.cpp
@@ -1,6 +1,8 @@
 #include "qthttpclient.h"
 
 #include <QtNetwork/QTcpSocket>
+#include <QUrl>
+#include <QUrlQuery>
 
 QtHttpClient::QtHttpClient(QTcpSocket *clientSocket, QObject *parent)
     : QObject(parent)
@@ -13,16 +15,40 @@ QtHttpClient::QtHttpClient(QTcpSocket *clientSocket, QObject *parent)
 }
 void QtHttpClient::handleReadyRead()
 {
-    if(m_ClientStream.device()->canReadLine())
+    QString response("<html><body>");
+    while(m_ClientStream.device()->canReadLine())
     {
         QString currentLine = m_ClientStream.readLine();
+        response.append("Your Request: " + currentLine + "<br />\n");
         if(currentLine.startsWith("GET "))
         {
+            QStringList getLineParts = currentLine.split(" ");
+            if(getLineParts.size() >= 2)
+            {
+                QString encodedUrl = getLineParts.at(1);
+                QUrl theUrl = QUrl::fromUserInput("http://dummydomain.com" + encodedUrl);
+                if(theUrl.isValid())
+                {
+                    response.append("Your url, decoded: " + theUrl.path() + "<br />\n");
 
-            m_ClientStream << "HTTP/1.0 200 OK\r\nContent-Type: text/html; charset=\"utf-8\"\r\n"
-                              "\r\n"
-                              "Your url: " << currentLine;
-            m_ClientStream.device()->close();
+                    QUrlQuery theUrlQuery(theUrl);
+                    QString xStr = theUrlQuery.queryItemValue("x");
+                    if(!xStr.isEmpty())
+                    {
+                        bool parseSuccess = false;
+                        int x = xStr.toInt(&parseSuccess);
+                        if(parseSuccess)
+                        {
+                            response.append("The value of x: " + QString::number(x) + "<br />\n");
+                        }
+                    }
+                }
+            }
         }
     }
+    response.append("</body></html>");
+    m_ClientStream << "HTTP/1.0 200 OK\r\nContent-Type: text/html; charset=\"utf-8\"\r\nConnection: close\r\nContent-Length: " << QString::number(response.length()) << "\r\n"
+                      "\r\n"
+                       << response;
+    m_ClientStream.device()->close();
 }
