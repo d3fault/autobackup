c7e55b54dba614a5a2480c604bd8c076c03baf4f
d3fault <d3fault@localhost.localdomain>
2015-03-16 00:22:56 -0700

    RecursiveGpgTools: Add gpgVerifyOneSigFromSigsFile.sh doesn't care about sig sizes and figures them out

--- Projects/RecursiveGpgTools/subtools/gpgVerifyOneSigFromSigsFile.sh
+++ Projects/RecursiveGpgTools/subtools/gpgVerifyOneSigFromSigsFile.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 if [ "$#" -lt 2 ]; then
-	echo "Usage: $0 relativeFilePathToVerify sigsFile [heightOfOneGpgSigInSigsFile=11]"
+	echo "Usage: $0 relativeFilePathToVerify sigsFile"
 	echo "Ex: $0 path/to/file.txt ~/path/to/sigfile.txt"
 	echo ""
 	echo "Note: relativeFilePathToVerify must be provided both in the format used in the sigs file (no leading ./), and it must also exist at that path relative to your working directory. So you should cd into the root of the directory you recursively sign/verify and then run this command from there"
 	exit 1
 fi
 
-heightOfOneGpgSigInSigsFile=11
-if [ "$#" -ge 3 ]; then
-	heightOfOneGpgSigInSigsFile="$3"
-fi
-grep "$1" -A "$heightOfOneGpgSigInSigsFile" "$2" | tail -n "$heightOfOneGpgSigInSigsFile" | gpg --verify - "$1" #TODOoptional: determine where the pgp sig ends dynamically without using the height like this
+desiredGpgSig0=`grep "$1" -A 2048 "$2"` #2048 _lines_ of sig data max xD
+numLinesUntilEndOfSig=`echo "$desiredGpgSig0" | grep -nm 1 "\-\-\-\-\-END PGP SIGNATURE\-\-\-\-\-" | cut -f1 -d:`
+numLinesOfSig=$((numLinesUntilEndOfSig-1)) # minus one to account for file path line
+echo "$desiredGpgSig0" | head -n "$numLinesUntilEndOfSig" | tail -n "$numLinesOfSig" | gpg --verify - "$1"
