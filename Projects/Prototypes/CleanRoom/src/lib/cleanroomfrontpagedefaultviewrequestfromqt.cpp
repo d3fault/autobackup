825ce017afa19140579abfec9eea3741ead7f5eb
d3fault <d3fault@d3fault.net>
2015-05-21 09:36:17 -0700

    QuickDirtyAutoBackupHalper2 Commit @Thu May 21 09:36:16 2015

--- Projects/Prototypes/CleanRoom/src/lib/cleanroomfrontpagedefaultviewrequestfromqt.cpp
+++ Projects/Prototypes/CleanRoom/src/lib/cleanroomfrontpagedefaultviewrequestfromqt.cpp
@@ -5,9 +5,8 @@
 CleanRoomFrontPageDefaultViewRequestFromQt::CleanRoomFrontPageDefaultViewRequestFromQt(CleanRoom *cleanRoom, QObject *objectCallback, const QString &slotCallback)
     : IFrontPageDefaultViewRequest(cleanRoom)
     , m_ObjectCallback(objectCallback)
-{
-    m_SlotCallback = slotCallback.toStdString();
-}
+    , m_SlotCallback(slotCallback.toStdString())
+{ }
 void CleanRoomFrontPageDefaultViewRequestFromQt::regainContextPossiblyInOtherThread_aka_respondActual(QVariantList responseArgs)
 {
     QMetaObject::invokeMethod(m_ObjectCallback, m_SlotCallback.c_str(), Q_ARG(QVariantList, responseArgs));
