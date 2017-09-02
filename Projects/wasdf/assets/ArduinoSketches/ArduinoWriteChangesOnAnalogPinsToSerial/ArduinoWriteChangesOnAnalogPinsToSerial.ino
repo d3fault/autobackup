760c24a7fc2f33303c7c02cf72c176ee0db63969
d3fault <d3fault@d3fault.net>
2017-09-02 13:39:20 -0700

    wasdf: refactor arduino sketch to consolidate calibrate/started code paths

--- Projects/wasdf/assets/ArduinoSketches/ArduinoWriteChangesOnAnalogPinsToSerial/ArduinoWriteChangesOnAnalogPinsToSerial.ino
+++ Projects/wasdf/assets/ArduinoSketches/ArduinoWriteChangesOnAnalogPinsToSerial/ArduinoWriteChangesOnAnalogPinsToSerial.ino
@@ -19,8 +19,10 @@ static const int HALF_THRESHOLD_FOR_NOTIFYING_OF_CHANGE = 2; //TODOoptional: thi
 #define WASDF_CHECKSUMMED_MESSAGE_HEADER_COMMADELIM ","
 #define WASDF_JSON_KEY_DEBUGMESSAGE "debugMessage"
 #define WASDF_JSON_KEY_COMMAND "command"
-#define WASDF_JSON_KEY_CALIBRATECOMMAND "calibrate"
 #define WASDF_JSON_KEY_STARTCOMMAND "start"
+#define WASDF_JSON_KEY_STOPCOMMAND "stop"
+#define WASDF_JSON_KEY_CONFIGURECOMMAND "configure"
+#define WASDF_JSON_KEY_RESETCONFIGURATIONCOMMAND "resetConfigurations"
 #define WASDF_JSON_KEY_ATRESTMIN "atRestMin"
 #define WASDF_JSON_KEY_ATRESTMAX "atRestMax"
 #define WASDF_JSON_KEY_ATRESTRANGES "fingersAtRestRanges"
@@ -96,33 +98,52 @@ bool newSensorValueHasChangedEnoughThatWeWantToReportIt(int oldSensorValue, int 
 
     return false;
 }
-bool sanitizingAnalogRead(int analogPinId, int *out_analogSensorValue)
-{
-    if(analogPinId == -1)
-        return false;
-    int rawSensorValue = analogRead(analogPinId);
-    if(rawSensorValue < MinAnalogPinValue || rawSensorValue > MaxAnalogPinValue)
-        return false;
-    *out_analogSensorValue = rawSensorValue;
-    return true;
-}
-class Finger_aka_AnalogPin //TODOmb: shared declaration between PC and Arduino?
+class AnalogPin_aka_FingerMb //TODOmb: shared declaration between PC and Arduino?
 {
 public:
-    explicit Finger_aka_AnalogPin()
-        : CurrentSensorValue(0)
+    explicit AnalogPin_aka_FingerMb()
+        : ChangeReportingSuppressedViaConfigurationCommand(false)
+        , CurrentSensorValue(0)
         , OldSensorValue(0)
     { }
-    void initialize(int analogPinId, int atRestMin, int atRestMax)
+    bool changeReportingSuppressedViaConfigurationCommand() const
+    {
+        return ChangeReportingSuppressedViaConfigurationCommand;
+    }
+    void setChangeReportingSuppressedViaConfigurationCommand(bool changeReportingSuppressedViaConfigurationCommand)
+    {
+        ChangeReportingSuppressedViaConfigurationCommand = changeReportingSuppressedViaConfigurationCommand;
+    }
+    void configure(int analogPinId = -1, int atRestMin = -1, int atRestMax = -1, bool changeReportingSuppressedViaConfigurationCommand = false)
     {
         IntPinId = analogPinId;
         AtRestMin = atRestMin;
         AtRestMax = atRestMax;
+        ChangeReportingSuppressedViaConfigurationCommand = changeReportingSuppressedViaConfigurationCommand;
         //TODOmb: sanitize at rest min/max here, fatal13 otherwise? idk I mean we've already verified the checksum of the data.. so.... yea idk tbh because it's still "foreign" data...? I guess this applies to pinId as well so fk idk lol what to assume... but FOR NOW at least I'll assume checksum valid == data valid (sane, within range, etc)
+    }
+    bool sanitizingAnalogRead(int *out_analogSensorValue)
+    {
+        if(IntPinId == -1)
+            return false;
+        int rawSensorValue = analogRead(IntPinId);
+        if(rawSensorValue < MinAnalogPinValue || rawSensorValue > MaxAnalogPinValue)
+            return false;
+        *out_analogSensorValue = rawSensorValue;
+        return true;
+    }
+    bool reportChangesInJsonObject(JsonObject *out_JsonObject)
+    {
+        if(fingerMoved())
+        {
+            (*out_JsonObject)[String(IntPinId)] = CurrentSensorValue;
+            return true;
+        }
+        return false;
     }
     bool fingerMoved()
     {
-         if(!sanitizingAnalogRead(IntPinId, &CurrentSensorValue))
+         if(!sanitizingAnalogRead(&CurrentSensorValue))
              return false;
         if(CurrentSensorValue <= AtRestMax && CurrentSensorValue >= AtRestMin)
             return false; //finger is within "at rest" range, so return false
@@ -139,15 +160,15 @@ public:
         }
         return false;
     }
-    ~Finger_aka_AnalogPin() { }
 
+    bool ChangeReportingSuppressedViaConfigurationCommand;
     int IntPinId;
     int CurrentSensorValue;
     int OldSensorValue;
     int AtRestMin;
     int AtRestMax;
 private:
-    explicit Finger_aka_AnalogPin(const Finger_aka_AnalogPin &other) { } //delete
+    explicit AnalogPin_aka_FingerMb(const AnalogPin_aka_FingerMb &other) { } //delete
 };
 
 void sendMessageToPc(const String &jsonString)
@@ -168,60 +189,6 @@ void sendMessageToPc(const String &jsonString)
     Serial.print(WASDF_CHECKSUMMED_MESSAGE_HEADER_COMMADELIM);
     Serial.print(jsonString);
 }
-
-class Hands
-{
-public:
-    void reportFingerMovementOverSerialPort()
-    {
-        int fingerIndexesThatMoved[NUM_FINGERS];
-        int currentIndexIntoFingerIndexesThatMoved = 0;
-
-        bool atLeastOneFingerMoved = false;
-        for(int i = 0; i < NUM_FINGERS; ++i)
-        {
-            if(Fingers[i].fingerMoved())
-            {
-                atLeastOneFingerMoved = true;
-                fingerIndexesThatMoved[currentIndexIntoFingerIndexesThatMoved] = i;
-                ++currentIndexIntoFingerIndexesThatMoved;
-            }
-        }
-        if(atLeastOneFingerMoved)
-        {
-            //even though the arduino2pc protocol is extremely simple atm and never changes from "analogPinId:newValue", I still want to convert it to json so I can 'bundle' all 10 finger movements into a single json object and (this is the key part) I send that jsonString over using the "SYNC,checksumOfSize,Size,checksumOfData,Data" protocol -- the same protocol used for pc2arduino comm (would be nice if they could share code). Yes I don't need to convert to json, BUT checksumming each "pinId:newValue" _line_ is inefficient as fuck, and "batching" them into 10 is a million times easier using json. thus, json is warranted
-
-            MyJsonBuffer jsonBuffer;
-            JsonObject &myObject = jsonBuffer.createObject();
-            for(int i = 0; i < currentIndexIntoFingerIndexesThatMoved; ++i)
-            {
-                const Finger_aka_AnalogPin &theFinger = Fingers[fingerIndexesThatMoved[i]];
-                String key_aka_PinId(theFinger.IntPinId);
-                myObject[key_aka_PinId] = theFinger.CurrentSensorValue;
-            }
-            String jsonString; //TODOoptimization: re-use. maybe need to clear() in between each printTo call?
-            myObject.printTo(jsonString);
-            sendMessageToPc(jsonString); //TODOreq: calibration mode should also call this sendMessageToPc function and send json over
-        }
-    }
-    void initializeFinger(int fingerArrayIndex, int analogPinId, int atRestMin, int atRestMax)
-    {
-        Fingers[fingerArrayIndex].initialize(analogPinId, atRestMin, atRestMax);
-    }
-private:
-    Finger_aka_AnalogPin Fingers[NUM_FINGERS];
-};
-
-struct Mode
-{
-    enum ModeEnum //TODOreq: I forget how to properly set up 'flags' that can be combined, this might be wrong (also calibrating can never be used with any other modes, but eh)
-    {
-          DoNothingMode = 0x000
-        , Calibrating = 0x001
-        , Sending10FingerMovementsMode = 0x010
-        , KeyboardAndMouseMode = 0x100
-    };
-};
 class MessageHeaderReader
 {
 private:
@@ -417,11 +384,61 @@ private:
     bool m_HaveReadMessageHeader;
     int m_NumBytesOfMessageLeft;
 };
+class AnalogPins_aka_FingersMb
+{
+public:
+    explicit AnalogPins_aka_FingersMb()
+    {
+        resetConfigurations();
+    }
+    void configureAnalogPinAtIndex(int analogPinArrayIndex, int analogPinId, int atRestMin, int atRestMax, bool changeReportingSuppressedViaConfigurationCommand)
+    {
+        AllAnalogPins_aka_FingersMb[analogPinArrayIndex].configure(analogPinId, atRestMin, atRestMax, changeReportingSuppressedViaConfigurationCommand);
+    }
+    void resetConfigurations()
+    {
+        for(int i = 0; i < NUM_ANALOG_INPUTS; ++i)
+        {
+            AnalogPin_aka_FingerMb &currentAnalogPin = AllAnalogPins_aka_FingersMb[i];
+            currentAnalogPin.configure(i);
+        }
+    }
+    void reportChangesOverSerialPort()
+    {
+        MyJsonBuffer jsonBuffer; //TODOoptimization: jit/lazy create the buffer, only whenever myObject gets a value inserted
+        JsonObject &myObject = jsonBuffer.createObject();
+
+        bool atLeastOneChanged = false;
+        for(int i = 0; i < NUM_ANALOG_INPUTS; ++i)
+        {
+            AnalogPin_aka_FingerMb &currentAnalogPin = AllAnalogPins_aka_FingersMb[i];
+            if(currentAnalogPin.changeReportingSuppressedViaConfigurationCommand())
+                continue;
+            if(currentAnalogPin.reportChangesInJsonObject(&myObject))
+                atLeastOneChanged = true;
+        }
+
+        if(!atLeastOneChanged) //TODOoptional: use myObject.keys().isEmpty
+            return;
+        String jsonString; //TODOoptimization: re-use. maybe need to clear() in between each printTo call?
+        myObject.printTo(jsonString);
+        sendMessageToPc(jsonString);
+    }
+    void suppressAll()
+    {
+        for(int i = 0; i < NUM_ANALOG_INPUTS; ++i)
+        {
+            AnalogPin_aka_FingerMb &currentAnalogPin = AllAnalogPins_aka_FingersMb[i];
+            currentAnalogPin.setChangeReportingSuppressedViaConfigurationCommand(true);
+        }
+    }
+private:
+    AnalogPin_aka_FingerMb AllAnalogPins_aka_FingersMb[NUM_ANALOG_INPUTS];
+};
 
 //Globals
-int CalibrationDataOldSensorValues[NUM_ANALOG_INPUTS];
-Mode::ModeEnum CurrentMode;
-Hands hands;
+bool Started;
+AnalogPins_aka_FingersMb analogPins_aka_FingersMb;
 String inputCommandBuffer;
 MessageReader *messageReader;
 
@@ -429,60 +446,10 @@ void setup()
 {
     messageReader = 0;
     Serial.begin(38400 /*, SERIAL_8O2 TODOreq: I want to try out "Odd" (that's an oh not a zero in between 8 and 2) parity and 2 stop bits to see if it improves reliability, but I keep getting an error that begin() only takes one argument wtf? maybe I need to upgrade? but it doesn't even look like debian stretch bumped the version at all (except a small patch). the define SERIAL_8O2 does in fact exist, so wtf?*/);
-    for(int i = 0; i < NUM_ANALOG_INPUTS; ++i)
-        CalibrationDataOldSensorValues[i] = 0;
-    CurrentMode = Mode::DoNothingMode;
+    Started = false;
     //commented this out because it's more memory conservative to allocate as needed: inputCommandBuffer.reserve(MAX_MESSAGE_SIZE); //TODOreq: calculate the max string length and reserve that much (or maybe a little more to be on the safe side). it can be determined because the values only take up so many characters. only thing is maybe the arduino mega 2560x1000 might have 999999999999 analog pins xDDDD, ya know what I mean??
     //inputCommandBuffer = new char(MAX_MESSAGE_SIZE); //TODOoptimization: never call new after takeoff. the md5 lib I'm gonna be using looks suspect af
     while(!Serial) ; //wait for serial port to connect. Needed for Leonardo/Micro only. I intentionally put the String::reserve command (and other cmds) in between this and Serial.begin() in case the connect happens asynchronously (idfk tbh, but it might), in which case I may have saved an entire millisecond!!!
-}
-bool analogPinSensorValueChangedDuringCalibration(int pinId, int indexIntoCalibrationDataOldSensorValues)
-{
-    int oldSensorValue = CalibrationDataOldSensorValues[indexIntoCalibrationDataOldSensorValues];
-    int newSensorValue;
-    if(!sanitizingAnalogRead(pinId, &newSensorValue))
-        return false;
-    if(newSensorValueHasChangedEnoughThatWeWantToReportIt(oldSensorValue, newSensorValue))
-    {
-        /*
-        Serial.print(pinId);
-        Serial.print(":");
-        Serial.println(newSensorValue);
-        */
-        CalibrationDataOldSensorValues[indexIntoCalibrationDataOldSensorValues] = newSensorValue;
-        return true;
-    }
-    return false;
-}
-void calibrationLoop()
-{
-    int CalibrationDataOldSensorValues_IndexesThatMoved[NUM_ANALOG_INPUTS];
-    int currentIndex_CalibrationDataOldSensorValues_IndexesThatMoved = 0;
-
-    bool atLeastOneAnalogPinSensorValueChanged = false;
-    for(int j = 0, i = A0; j < NUM_ANALOG_INPUTS; ++j, ++i)
-    {
-        if(analogPinSensorValueChangedDuringCalibration(i, j))
-        {
-            atLeastOneAnalogPinSensorValueChanged = true;
-            CalibrationDataOldSensorValues_IndexesThatMoved[currentIndex_CalibrationDataOldSensorValues_IndexesThatMoved] = i;
-            ++currentIndex_CalibrationDataOldSensorValues_IndexesThatMoved;
-        }
-    }
-    if(atLeastOneAnalogPinSensorValueChanged)
-    {
-        MyJsonBuffer jsonBuffer;
-        JsonObject &myObject = jsonBuffer.createObject();
-        for(int i = 0; i < currentIndex_CalibrationDataOldSensorValues_IndexesThatMoved; ++i)
-        {
-            int indexIntoCalibrationDataOfAnalogPinThatMoved = CalibrationDataOldSensorValues_IndexesThatMoved[i];
-            int newSensorValue = CalibrationDataOldSensorValues[indexIntoCalibrationDataOfAnalogPinThatMoved];
-            myObject[String(A0 + indexIntoCalibrationDataOfAnalogPinThatMoved)] = newSensorValue;
-        }
-        String jsonString; //TODOoptimization: re-use. maybe need to clear() in between each printTo call?
-        myObject.printTo(jsonString);
-        sendMessageToPc(jsonString);
-    }
 }
 int parseAndSanitizeAnalogPinIdFromForeignString(const String &foreignString)
 {
@@ -513,13 +480,20 @@ void processInputCommandString(const String &inputCommandString)
     debugMessage += inputCommandString;
     sendDebugMessage(debugMessage);
 
-    if(command == WASDF_JSON_KEY_CALIBRATECOMMAND)
+    if(command == WASDF_JSON_KEY_STARTCOMMAND)
     {
-        CurrentMode = Mode::Calibrating;
+        Started = true;
     }
-    else if(command == WASDF_JSON_KEY_STARTCOMMAND)
+    else if(command == WASDF_JSON_KEY_STOPCOMMAND)
     {
-        CurrentMode = Mode::Sending10FingerMovementsMode;
+        Started = false;
+    }
+    else if(command == WASDF_JSON_KEY_CONFIGURECOMMAND)
+    {
+        //first set all analog pins to SUPPRESS their change reporting
+        analogPins_aka_FingersMb.suppressAll();
+        //then, using the received json config, re-enable the analog pins desired (this is done below, the 'false' param sent to configureAnalogPinAtIndex) -- this is easier than "figuring out which analog pins aren't desired" (and suppressing those only), and does the same thing
+
         //the "used analog pin IDs" (connected to the 10 fingers) is sent as args (TODOreq: make sure PC isn't sending "0-9" but is instead sending the analog pin IDs corresponding to fingers 0-9
         //retrive atRestMin/Max from json, corresponding to the analog pin IDs being used
         const JsonObject &atRestPositionsJsonObject = rootJsonObject[WASDF_JSON_KEY_ATRESTRANGES];
@@ -537,7 +511,7 @@ void processInputCommandString(const String &inputCommandString)
                 fatalErrorBlinkPin13(8);
                 return;
             }
-            hands.initializeFinger(i, analogPinId, atRestMin, atRestMax);
+            analogPins_aka_FingersMb.configureAnalogPinAtIndex(i, analogPinId, atRestMin, atRestMax, false);
             ++it, ++i;
         }
         if(i < NUM_FINGERS)
@@ -547,6 +521,10 @@ void processInputCommandString(const String &inputCommandString)
         }
         debugMessage = "started";
         sendDebugMessage(debugMessage);
+    }
+    else if(command == WASDF_JSON_KEY_RESETCONFIGURATIONCOMMAND)
+    {
+        analogPins_aka_FingersMb.resetConfigurations();
     }
     else
     {
@@ -561,21 +539,8 @@ void processInputCommandString(const String &inputCommandString)
 }
 void sendOutputIfAny()
 {
-    switch(CurrentMode) //TODOreq: this switch case doesn't support simultaneous 10-fingers and keyboardMouse modes
-    {
-        case Mode::Sending10FingerMovementsMode:
-            hands.reportFingerMovementOverSerialPort(); //tell, don't ask ;-)
-        break;
-        case Mode::KeyboardAndMouseMode:
-            //TODOreq. I'm starting to lean towards synthing kb/mouse on pc side instead of using the micro/leonardo built-in approach, for various reasons (serialization/correct-replaying of the input command stream greatly simplified being the biggest)
-        break;
-        case Mode::Calibrating:
-            calibrationLoop();
-        break;
-        case Mode::DoNothingMode:
-            //I used to hav a delay(50) here, until I learned that Serial has internal buffer size of only 64 bytes
-        break;
-    }
+    if(Started)
+        analogPins_aka_FingersMb.reportChangesOverSerialPort(); //tell, don't ask ;-)
 }
 void receiveInputIfAny()
 {
@@ -590,6 +555,7 @@ void receiveInputIfAny()
     {
         delete messageReader; //OOM optimization
         messageReader = 0;
+
         processInputCommandString(inputCommandBuffer);
     }
 }
