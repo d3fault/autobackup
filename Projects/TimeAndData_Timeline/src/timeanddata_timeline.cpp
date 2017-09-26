68b8f48b8f76282a188d5d76d8046b235dbfab6d
d3fault <d3fault@d3fault.net>
2017-09-25 17:00:03 -0700

    TimeAndData_Timeline: add append. it's extremely primitive/quick because I'm PREDICTING I'll have to refactor it. but it works. I mean I haven't tried to compile yet obviously but it should definitely work lol (aside from dumb bugs during compilation (the kind we IGNORE becaues the compiler will remind us <3))

--- Projects/TimeAndData_Timeline/src/timeanddata_timeline.cpp
+++ Projects/TimeAndData_Timeline/src/timeanddata_timeline.cpp
@@ -1,11 +1,19 @@
 #include "timeanddata_timeline.h"
 
+#include <QCoreApplication> //for QSettings global config
+#include <QSettings> //TODOmb: migrate away from the simplicity of a QSettings-based application database?
 #include <QDateTime>
+#include <QCryptographicHash>
 
 //TODOreq: there is no delete, only append
 TimeAndData_Timeline::TimeAndData_Timeline(QObject *parent)
     : QObject(parent)
-{ }
+{
+    QCoreApplication::setOrganizationName("TimeAndData_TimelineOrganization");
+    QCoreApplication::setOrganizationDomain("TimeAndData_TimelineDomain");
+    QCoreApplication::setApplicationName("TimeAndData_Timeline");
+    QSettings::setDefaultFormat(QSettings::IniFormat); //TODOreq: search EVERY project I've coded for the use of the above 3 configs (setOrgName, setDomainName, setAppName) and make sure that this setDefaultFormat is always called there too (unless the app doesn't warrant it, but I think most do. Ini files are just much more portable than "registrty edits" on that one gross operating system. Hell I think it should default to Ini format on all platforms and the Native formats should be opted INTO (since they [can] _SUCK_) TODOmb: file bugreport to default to Ini (or pull request)
+}
 void TimeAndData_Timeline::appendJsonObjectToTimeline(const QJsonObject &data)
 {
     //TO DOnereq(read huge comment below): should I have time passed in to me or should I determine it myself here/now?
@@ -16,4 +24,15 @@ void TimeAndData_Timeline::appendJsonObjectToTimeline(const QJsonObject &data)
     //it's STILL possible to get dupe timestamps of course. you know what fuck it I like year/month/day folderization, then numMSecElapsedToday_sha512orwhatever.bin -- ex: 2017/9/23/69420_09f9deadbeefbabesha512hex.bin
     //MAYBE, if after analyzing data the type can be DETERMINED to be plaintext, we use a .txt extension for it and if not a .bin extension? or we can use a .json if determined to be .json? oh where the fuck does it end??? binary/text is one thing, but nah json nah (but then again json is "special" as it's my internal type etc, so maybe ONE exception for json. bin=unknown, txt=txt-not-json, json=json). wait no it is ALWAYS json. we task the USER OF this lib with being json-safe, b64-encoding if needed. so yea there is only ONE extension type xD: .json <3. note that even though it's json and human readable, it's still intended to be READ-ONLY (modifying would invalidate the sha512!! use StupidKeyValueContentTracker if you want to "MODIFY" (revise) a "file". TODOreq: there is a data race maybe if 2+ StupidKeyValueContentTracker instances share a Timeline instance and call append simultaneously. one of them might "mutate" the other mutation (rather than the state before it), which is uninntended. TODOreq: we need a "appendAtExactlyThisPositionOrFail" (the position is polled from Timeline, just before commit. so it will almost always go through, but the occassional data races are prevented! we just error out, possibly retrying (now instead mutating the mutation (or rather, it's state after applied) that JUSt beat us to the append) depending on the intent and if there are conflicts)
     //man this blew up in my face, I'm glad it was kept as simple as it is though :). just time and data :)
+
+    //I've thought more on this and decided not to do year/month/day/msecElapsedSinceStartOfDay.json, yet
+    //nor will I use a cryptographic hash, yet
+    //I'm going to do something quick and simple and PREDICT that I will refactor it when I implement readKey and/or readTimeline(what to use as input for timeline? (this will "come to me" when I'm coding readKey probably))
+    QSettings settings;
+
+    QJsonDocument jsonDoc(data);
+    QByteArray jsonByteArray = jsonDoc.toJson(QJsonDocument::Compact/*TODOreq: Indented if ever moved out of QSettings*/);
+    QByteArray hash = QCryptographicHash::hash(jsonByteArray, QCryptographicHash::Sha3_256);
+    QString hashOfTimeAndData(hash.toHex());
+    settings.setValue(hashOfTimeAndData, data); //fukken ayy, <3 QVariant + QJsonObject integration <3 <3 Qt
 }
